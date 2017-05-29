363f6a30bd821e05f664bda1dc97c003cc4e32b4
Phan Van Linh <phanvanlinh.94vn@gmail.com>
2017-05-28 18:35:52 -0700

    Update projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -70,24 +70,21 @@ export class AppComponent {
             title: 'A Full Stack Developer with nearly 3 years of experience in Software Development, Project Maintenance and Project Publication.',
         },
         {
-            title: 'Good understanding of full mobile and software development life cycles, UI, UX, web analytics, Git control and Agile methodology.',
+            title: 'Good understanding of full mobile and software development life cycles, UI, UX, integrating API, sensor, supporting mutiple device, web analytics, Git control and Agile methodology.',
         },
         {
-          title: 'Understanding of Web architecture design, MVC architecture, Restful API',
+          title: 'Understanding of Web architecture design, MVC architecture, database design, authentication, Restful API.',
         },
         {
-            title: 'Good in object-oriented design, data structures, problem solving/analysis and debugging.',
-        },
-        {
-            title: 'Experiences working with MVC, MVP, MVVM with some design patterns',
+            title: 'Good in object-oriented design, data structures, design pattern, problem solving/analysis and debugging.',
         },
         {
             title: 'Able to build a complete moblie application: Server API (PHP+Mysql or NodeJs+MongoDB), client (Android/IOS), intergrate advertisment (StartApp, MobileCore, ...) and public to store (PlayStore or AppStore).',
         },
         {
-            title: 'Familiar with multimedia, healthcare, social network, map, locate application. Experiences in integrate social network(Facebook, Google, Twitter, Instagram, SoundCloud, Linkedin to mobile application.',
+            title: 'Familiar with multimedia, healthcare, social network, map, locate, camera application. Experience in integrate social network (Facebook, Google, Twitter, Instagram, SoundCloud, Linkedin to mobile application.',
         }, {
-            title: 'Currently, my English skill is still not very good but I am pretty sure that I can and want to work in the enviroment where using full English.'
+            title: 'Able and want to work in the enviroment where using full English.'
         }
     ];
 
@@ -122,19 +119,19 @@ export class AppComponent {
             },
             time: '10/2014-02/2015',
             position: 'Intern Web Developer',
-            description: 'Had a chance to learn a lot about English, soft skill and web Development. \nCommunicated with potential customers to manage expectations. And builded sameple website base on the expectations.\nUsed Git for control code, Trello for control tasks and follow Agile methodology',
+            description: 'Had a chance to learn a lot about English, soft skill and Web development. \nCommunicated with potential customers to manage expectations. And builded sameple website base on the expectations.\nUsed Git for control code, Trello for control tasks and follow Agile methodology',
         }
     ];
 
     top_skills = [{
             title: 'Android',
             experience: '1.5 years',
-            description: 'Good understanding of mobile development life cycle.\nExperience in application performance, optimize code/layout, manager application memory, protect data using encrypt and reduce apk size.\nDeveloping against multiple devices and custom UI for native Android component.\nExperience in public application and integrate advertisment to PlayStore'
+            description: 'Familiar with Android frameworks, devices, OS, libraries.\nExperience in using data-binding, RxAndroid, Dagger, EvenBus, Firebase, Realm, SQLite. \nExperience in application performance, optimize code/layout, manager application memory and reduce apk size.\nExperience in developing against multiple devices/OS and custom UI, animations for native Android component. \nExperience in public application to PlayStore and integrate advertisment'
         },
         {
             title: 'iOS',
             experience: '8 months',
-            description: 'Experienced with iOS SDK, iOS components, auto-layout, navigationm, design Universal application.\nExperience in public application to AppStore'
+            description: 'Experienced with iOS SDK, iOS components, auto-layout, navigationm, design Universal application.\nExperience in integrating social networks (FB, GG, TW, SC, IG) and web analytic (Crashlytics). \nExperience in public application to AppStore'
         },
         {
             title: 'Web (PHP)',
@@ -247,7 +244,7 @@ export class AppComponent {
             time: '2017',
             icon: 'assets/img/project/kyoriosuku.png',
             link: 'https://play.google.com/store/apps/details?id=jp.co.mapion.android.app.kyorisoku',
-            about: '',
+            description: 'A map application that help calculate distance, elevation, calries burn between 2 or many places. It supports writing memo for every place, save and share this memo or route to another person after login',
         },
         {
             name: 'Arukuto',
@@ -257,7 +254,7 @@ export class AppComponent {
             time: '2016,2017',
             icon: 'assets/img/project/arukuto.png',
             link: 'https://play.google.com/store/apps/details?id=com.mapion.android.arukuto',
-            about: '',
+            description: 'A healthy application that makes people walk more. People should walk to some specific place (NPC) and walk to enough step for upgrading level or receive some real gift',
         },
         {
             name: 'EarCrush',
@@ -266,8 +263,8 @@ export class AppComponent {
             company: 'Evizi',
             time: '2015, 2016',
             icon: 'assets/img/project/earcrush.png',
-            link: '',
-            about: '',
+            link: 'https://itunes.apple.com/us/app/earcrush/id1064873601?mt=8',
+            description: 'Like a social network application, it contains all basic function of a social network like (new feed, profile, friends, chatting). Moreover, it support view/post new status to all social network (FB, GG, TW, SC, IG) after we connected.'
         },
         {
             name: 'Itson',
@@ -277,7 +274,7 @@ export class AppComponent {
             time: '2016',
             icon: 'assets/img/project/itson.png',
             link: 'http://www.itsoninc.com/',
-            about: '',
+            description: 'A application for Network Service Provider, each Network Service Provider is a tenant. Using application, the tenant can allow their user recharge, top up, using some plan/promotion, view account details insted of sending a message.',
         },
         {
             name: 'Weaved',
@@ -287,7 +284,7 @@ export class AppComponent {
             time: '2015',
             icon: 'assets/img/project/weaved.png',
             link: 'https://itunes.apple.com/us/app/weaved/id937372469?mt=8',
-            about: '',
+            description: 'A application that show/stream of all managed camera. ',
         },
         {
             name: 'Yoics',
@@ -297,7 +294,7 @@ export class AppComponent {
             time: '2015',
             icon: 'assets/img/project/yoics.png',
             link: 'https://itunes.apple.com/us/app/look-camera-viewer/id768563141?mt=8',
-            about: '',
+            description: 'A application that help us control, config camera',
         },
         {
             name: 'Buzzr',
@@ -307,7 +304,7 @@ export class AppComponent {
             time: '2015',
             icon: 'assets/img/project/buzzr.png',
             link: '',
-            about: '',
+            description: 'A social network for only people who using car. It contains all basic function of a social network like (new feed, profile, friends, chatting). Moreover, it contain a map in main screen where we can see all closer people.',
         },
         {
             name: 'Fresh Food',
@@ -317,7 +314,7 @@ export class AppComponent {
             time: '2015',
             icon: 'assets/img/project/freshfood.png',
             link: 'https://medium.com/totoro-design-in-english/fresh-food-project-design-process-b0c9fbcc9a77',
-            about: '',
+            description: 'Web project that may help housewife who do not help much time. The main idea is creating a service that will prepare (clean/cut) raw food and a recipe. Then the housewife can buy it and cook faster',
         }
     ];
 
@@ -367,7 +364,7 @@ export class AppComponent {
             image: '',
             icon: 'assets/img/social/github.png',
             content: 'Not much achieve. Currently, I use Github for manage some of my demo projects',
-            time: '03/2017',
+            time: '10/2015',
             link: 'https://github.com/PhanVanLinh'
         },
         {
@@ -375,7 +372,7 @@ export class AppComponent {
             image: '',
             icon: 'assets/img/social/hackerrank.png',
             content: 'I achieved 306 points and 236 hackos by resolve more than 20 algorithm tests',
-            time: '10/2015',
+            time: '03/2017',
             link: 'https://www.hackerrank.com/phanvanlinh_94vn?hr_r=1'
         },
     ];
@@ -383,7 +380,7 @@ export class AppComponent {
     volunteerisms = [{
         place: 'Passerelles numériques Vietnam',
         time: '2015 - Present',
-        details: 'Each year, from September to May, I always support students at my old school learning about C, Java, Android and English in 2h in the evening (6h-8h) for onece or twice times a week ',
+        details: 'Each year, from September to May, I always support students at my old school learning about C, Java, Android and English in the evening (18h-20h) for onece or twice times a week ',
         link: 'https://www.passerellesnumeriques.org/vi/cac-trung-tam/vietnam/'
     }];
 
@@ -403,7 +400,6 @@ export class AppComponent {
             icon: 'fa fa-fw fa-linkedin'
         }
     ];
-
 
     hobbies = [{
             title: 'Programming',
